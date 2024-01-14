a38daa2d0361af83b2cfa837cb173543284646d1
John Kilima <jkbundala@gmail.com>
2024-01-15 00:09:46 +0300

    Create Simple SpringBott REST API - @GetMapping handling get requests

--- src/main/java/dev/kilima/springbootrestapi/controller/HelloWorldController.java
+++ src/main/java/dev/kilima/springbootrestapi/controller/HelloWorldController.java
@@ -1,5 +1,25 @@
 package dev.kilima.springbootrestapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.ResponseBody;
+/*
+ * @Controller converts the class to a springboot controller 
+ * And @ResponseBody converts response from java objects to JSON
+ * @RestController - combines both properties of @Controller and @ResponseBody
+ */
+
+//@Controller
+//@ResponseBody
+@RestController
 public class HelloWorldController {
 
+	// HTTP GET REQUEST
+	// http://localhost:8080/hello-world
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
 }
