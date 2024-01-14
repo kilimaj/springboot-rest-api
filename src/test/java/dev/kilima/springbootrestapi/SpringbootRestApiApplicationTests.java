aab40c646baaab5d7596c0f0b52f7b0c469ae57c
John Kilima <jkbundala@gmail.com>
2024-01-14 18:56:28 +0300

    Initial springboot project generated

---
+++ src/test/java/dev/kilima/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kilima.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
