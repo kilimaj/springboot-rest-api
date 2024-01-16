97b96d966a7c1b0eb69c143a29b3cdaaefe0c348
JOHN KILIMA <jkbundala@gmail.com>
2024-01-16 17:04:10 +0300

    SpringBoot REST API returns Java Bean - Creating parametrised constructor

--- src/main/java/dev/kilima/springbootrestapi/bean/Student.java
+++ src/main/java/dev/kilima/springbootrestapi/bean/Student.java
@@ -4,4 +4,15 @@ public class Student {
 	private int id;
 	private String firstName;
 	private String lastName;
+	
+	public Student(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	
+	
+	
 }
