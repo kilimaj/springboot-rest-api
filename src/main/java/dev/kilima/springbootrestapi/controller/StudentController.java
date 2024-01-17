1d0d50cb59285826b06906aa290075a5f6aa8cfb
JOHN KILIMA <jkbundala@gmail.com>
2024-01-17 12:22:27 +0300

    Create SpringBoot REST API returns List

--- src/main/java/dev/kilima/springbootrestapi/controller/StudentController.java
+++ src/main/java/dev/kilima/springbootrestapi/controller/StudentController.java
@@ -1,5 +1,8 @@
 package dev.kilima.springbootrestapi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +11,20 @@ import dev.kilima.springbootrestapi.bean.Student;
 @RestController
 public class StudentController {
 
-	//http://localhost:8080/student
+	// http://localhost:8080/student
 	@GetMapping("student")
 	public Student getStudent() {
 		Student student = new Student(1, "John", "Kilima");
 		return student;
+	}
+
+	@GetMapping("students")
+	public List<Student> getStudents() {
+		List<Student> students = new ArrayList<>();
+		students.add(new Student(1, "John", "Kilima"));
+		students.add(new Student(2, "Mabula", "Kilima"));
+		students.add(new Student(3, "Jean", "Bundala"));
+		students.add(new Student(4, "Barnabas", "Kilima"));
+		return students;
 	}
 }
