7a0d17d414a02316b6bfb68fd730765c7c2b82bd
JOHN KILIMA <jkbundala@gmail.com>
2024-01-17 08:15:07 +0300

    Spring Boot REST API returns Java Bean - Generating getters and setters

--- src/main/java/dev/kilima/springbootrestapi/bean/Student.java
+++ src/main/java/dev/kilima/springbootrestapi/bean/Student.java
@@ -11,6 +11,30 @@ public class Student {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 	
 	
 	
