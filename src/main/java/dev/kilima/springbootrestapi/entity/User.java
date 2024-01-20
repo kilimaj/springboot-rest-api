53163e88c193a7a7e0c6c5799038ba4e79b4c70b
JOHN KILIMA <jkbundala@gmail.com>
2024-01-20 16:50:33 +0300

    Create User JPA Entity

---
+++ src/main/java/dev/kilima/springbootrestapi/entity/User.java
@@ -0,0 +1,30 @@
+package dev.kilima.springbootrestapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	@Column(nullable = false)
+	private String lastNmae;
+	@Column(nullable = false, unique = true)
+	private String email;
+}
