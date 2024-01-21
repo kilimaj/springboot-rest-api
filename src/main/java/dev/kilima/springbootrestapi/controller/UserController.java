9f495d64a4516fb28dddad081a9bd2113ecea798
JOHN KILIMA <jkbundala@gmail.com>
2024-01-21 09:52:23 +0300

    Build Update User REST API

--- src/main/java/dev/kilima/springbootrestapi/controller/UserController.java
+++ src/main/java/dev/kilima/springbootrestapi/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,13 @@ public class UserController {
 	public ResponseEntity<List<User>> getAllUsers(){
 		List<User> users = userService.getAllUsers();
 		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+	
+	//Build update User REST API
+	@PutMapping("update/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable("id") Long userId,@RequestBody User user){
+		user.setId(userId);
+		User updatedUser = userService.updateUser(user);
+		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
 	}
 }
