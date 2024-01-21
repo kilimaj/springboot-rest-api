ceeb205dc765849136624dda8e59fad70ee13daf
John Kilima <jkbundala@gmail.com>
2024-01-21 17:37:02 +0300

    Create a User Mapper to keep common logic

---
+++ src/main/java/dev/kilima/springbootrestapi/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package dev.kilima.springbootrestapi.mapper;
+
+import dev.kilima.springbootrestapi.dto.UserDto;
+import dev.kilima.springbootrestapi.entity.User;
+
+public class UserMapper {
+
+	// Convert User JPA Entity into User DTO
+	public static UserDto maoToUserDto(User user) {
+		UserDto userDto = new UserDto(
+				user.getId(),
+				user.getFirstName(),
+				user.getLastName(),
+				user.getEmail()
+			);
+		return userDto;
+	}
+	
+	// Convert userDto into user Entity
+	public static User mapToUser(UserDto userDto) {
+		User user = new User(
+			userDto.getId(),
+			userDto.getFirstName(),
+			userDto.getLastName(),
+			userDto.getEmail()
+		);
+		return user;
+	}
+}
